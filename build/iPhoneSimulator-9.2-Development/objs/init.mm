27b45ccac2904112af5522f17e5569f9618479b8
Amkissler <amkissler@yahoo.com>
2016-02-02 21:58:49 -0500

    s

---
+++ build/iPhoneSimulator-9.2-Development/objs/init.mm
@@ -0,0 +1,50 @@
+#import <Foundation/Foundation.h>
+
+extern "C" {
+    void ruby_sysinit(int *, char ***);
+    void ruby_init(void);
+    void ruby_init_loadpath(void);
+    void ruby_script(const char *);
+    void ruby_set_argv(int, char **);
+    void rb_vm_init_compiler(void);
+    void rb_vm_init_jit(void);
+    void rb_vm_aot_feature_provide(const char *, void *);
+    void *rb_vm_top_self(void);
+    void rb_define_global_const(const char *, void *);
+    void rb_rb2oc_exc_handler(void);
+    void rb_exit(int);
+void MREP_5BD31BE2C09B48339874A1AC8AE2D82A(void *, void *);
+void MREP_D47186CABE034A81B39259FCD82C9072(void *, void *);
+void MREP_F800D45D470845D6924A68D3BEF09263(void *, void *);
+}
+
+extern "C"
+void
+RubyMotionInit(int argc, char **argv)
+{
+    static bool initialized = false;
+    if (!initialized) {
+	ruby_init();
+	ruby_init_loadpath();
+        if (argc > 0) {
+	    const char *progname = argv[0];
+	    ruby_script(progname);
+	}
+#if !__LP64__
+	try {
+#endif
+	    void *self = rb_vm_top_self();
+rb_define_global_const("RUBYMOTION_ENV", @"development");
+rb_define_global_const("RUBYMOTION_VERSION", @"4.8");
+MREP_5BD31BE2C09B48339874A1AC8AE2D82A(self, 0);
+MREP_D47186CABE034A81B39259FCD82C9072(self, 0);
+MREP_F800D45D470845D6924A68D3BEF09263(self, 0);
+#if !__LP64__
+	}
+	catch (...) {
+	    rb_rb2oc_exc_handler();
+	}
+#endif
+	initialized = true;
+    }
+}
